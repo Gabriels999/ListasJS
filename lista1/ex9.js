00b42d359faafbb2406b8069686fa803a965b0c6
Gabriel Santos <gab.tec97@gmail.com>
2022-08-11 10:04:19 -0300

    Resolve ex9 - lista1

---
+++ lista1/ex9.js
@@ -0,0 +1,27 @@
+const numero = 7;
+if (numero > 7 || numero < 1) {
+  console.log("Número inválido.");
+} else {
+  switch (numero) {
+    case 1:
+      console.log("Domingo");
+      break;
+    case 2:
+      console.log("Segunda-feira");
+      break;
+    case 3:
+      console.log("Terça-feira");
+      break;
+    case 4:
+      console.log("Quarta-feira");
+      break;
+    case 5:
+      console.log("Quinta-feira");
+      break;
+    case 6:
+      console.log("Sexta-feira");
+      break;
+    case 7:
+      console.log("Sábado");
+  }
+}
